079a1d2472f594b3455715e78c7f695a84b49324
abdala9512 <miguel.arquez12@gmail.com>
2021-01-08 15:56:32 -0500

    repository update

--- env_update.sh
+++ env_update.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+update_repository (){
+    git pull origin ${1:-main}
+    py_version = $(python --version)
+    if [[ $py_version = 'Python 3.8.5' ]]; then
+        echo "correct"
+    else
+        echo "incorrect"
+    fi
+}
