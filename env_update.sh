b8c70e609aea6ff64fd0a0908333b78eec26d4e9
abdala9512 <miguel.arquez12@gmail.com>
2021-01-08 22:20:55 -0500

    python module install function

--- env_update.sh
+++ env_update.sh
@@ -1,23 +1,34 @@
 #!/bin/bash
 
+python_package_install () {
+    package=$1
+    if [[  $( pip freeze | grep -w $package ) ]]; then
+        pip install $package
+    fi
+
+}
+
 update_repository (){
 
-    py_version = $( python --version )    
+    py_version=$( python -c 'import sys; print(sys.version_info[:3])' )
+    git pull origin ${1:-main}
+    if [[ "$py_version" = '(3, 8, 5)' ]]; then
+        echo -e "Incorrect python environment. Make sure you're working with Python 3.8.5"
+    else
+        echo "incorrect"
+    fi
+  
     git_remote = $( git remote -v )
 
     if [[ -f requirements.txt ]]; then
         for lib in $( cat requirements.txt )
         do
-            echo $lib
+            python_package_install $lib
         done
     else
         echo "requirements.txt file not found"
     fi
-    git pull origin ${1:-main}
-    
-    if [[ $py_version = 'Python 3.8.5' ]]; then
-        pip install
-    else
-        echo "incorrect"
-    fi
+
 }
+
+export -f update_repository
